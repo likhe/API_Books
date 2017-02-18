5b0fae07e63e9b879350301828bf00e91ac3a496
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-19 00:30:14 +0100

    test reactjs

--- BookFLO/app.js
+++ BookFLO/app.js
@@ -1,32 +1,7 @@
-var myApp = angular.module('routing', []).config(function($sceDelegateProvider) {
-  $sceDelegateProvider.resourceUrlWhitelist([
-    // Allow same origin resource loads.
-    'self',
-    'https://www*.googleapis.com/**',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'http://srv*.assets.example.com/**'
-  ]);
-
-  // The blacklist overrides the whitelist so the open redirect here is blocked.
-  $sceDelegateProvider.resourceUrlBlacklist([
-    'http://myapp.example.com/clickThru**'
-  ]);
-});
-
-myApp.controller('ExampleController', ['$scope', '$http', "$q", function($scope, $http, $q) {
-    console.log("salut");
-
-    $http({
-        method: 'GET',
-        url: 'https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ'
-    }).then(function successCallback(response, data, status) {
-        console.log("response = ", response);
-        console.log("data = ", data);
-    }, function errorCallback(response) {
-        //add by ben
-        console.log("response errorCallback = ", response);
-        console.log("error can't get the JSON file from the server");
-        document.getElementById("test").innerHTML = "Erreur lors de l'appel du json"
-    });
-
-}]);
+function handleResponse(response) {
+for (var i = 0; i < response.items.length; i++) {
+  var item = response.items[i];
+  // in production code, item.text should have the HTML entities escaped.
+  document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+}
+}
