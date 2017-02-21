857060e72524e998bc191259178514ad4df6647e
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-02-21 02:11:03 +0100

    update

--- BookFLO/app.js
+++ BookFLO/app.js
@@ -28,24 +28,25 @@ myApp.controller('Controller', ['$scope', '$http', "$q", function($scope, $http,
 
 
     function urlmaker() {
-        let jsonurl = "https://www.googleapis.com/books/v1/volumes?q=";
+        let key = "&key=AIzaSyCsQR7F04zJVfUL4trC4XFh7tEwLwjt4DY" // google api key for monitoring
+        let jsonurl = "https://www.googleapis.com/books/v1/volumes?q="; // url base
+        let args = ""; // arguments in case we want a more specific search
+        let newarray = [];
+        let formatedstring = "";
 
         inputvalue = document.getElementById("search").value;
         console.log("inputvalue = ", inputvalue);
-        let newarray = [];
-        let formatedstring = "";
-        let key = "&key=AIzaSyCsQR7F04zJVfUL4trC4XFh7tEwLwjt4DY"
 
         newarray = inputvalue.split(" ");
         console.log("newarray :", newarray);
 
-        formatedstring = newarray.join('%20');
+        formatedstring = newarray.join('%20'); //join all index values in a string and put a space char between them
         console.log("formatedstring :", formatedstring);
 
         jsonurl += formatedstring;
-        console.log("json url write by the function :", jsonurl);
-&key=yourAPIKey
-        getjson(jsonurl);
+        console.log("json url write by the function :", jsonurl); // url done
+
+        getjson(jsonurl); //sending the formatted url to the getjson function
 
     };
 
@@ -69,10 +70,7 @@ myApp.controller('Controller', ['$scope', '$http', "$q", function($scope, $http,
             console.log("Date = ", books[0].volumeInfo.publishedDate);
             console.log("smallThumbnail = ", books[0].volumeInfo.imageLinks.smallThumbnail);
 
-
-
-
-            creatinglist();
+            validitycheck();
         }, function errorCallback(response) {
             console.log("error can't get the JSON file from the server", response);
             document.getElementById("result").innerHTML = "Erreur lors de l'appel du json";
@@ -80,32 +78,74 @@ myApp.controller('Controller', ['$scope', '$http', "$q", function($scope, $http,
         });
     };
 
+
+    function validitycheck() {
+        console.log("validity check started");
+        for (var i = 0; i < books.length; i++) {
+
+          let smallThumbnail = books[i].volumeInfo.imageLinks.smallThumbnail,
+          title = books[i].volumeInfo.title,
+          author = books[i].volumeInfo.authors,
+          isbn = books[i].volumeInfo.industryIdentifiers[0],
+          publisher = books[i].volumeInfo.publisher,
+          publisherDate = books[i].volumeInfo.publishedDate;
+
+
+            if (smallThumbnail == undefined) {
+                books[i].volumeInfo.imageLinks.smallThumbnail = "N/C";
+            }
+            if (title == undefined) {
+                books[i].volumeInfo.title = "N/C";
+            }
+            if (author == undefined) {
+                books[i].volumeInfo.authors = "N/C";
+            }
+            if (isbn == undefined) {
+                books[i].volumeInfo.industryIdentifiers[0].identifier = "N/C";
+            }
+            if (publisher == undefined) {
+                books[i].volumeInfo.publisher = "N/C";
+            }
+            if (publisherDate == undefined) {
+                books[i].volumeInfo.publishedDate = "N/C";
+            } else {
+                console.log("check done");
+            }
+        }
+        creatinglist();
+    }
+
+
+
+
     function creatinglist() {
+        $scope.results = []; // array reset
+
         console.log("making an array with info we needs");
         for (var i = 0; i < books.length; i++) {
-            $scope.results.push({smallThumbnail:books[i].volumeInfo.imageLinks.smallThumbnail, title: books[i].volumeInfo.title,author: books[i].volumeInfo.authors[0], isbn: books[i].volumeInfo.industryIdentifiers[0].identifier, publisher: books[i].volumeInfo.publisher, publisherDate: books[i].volumeInfo.publishedDate})
 
 
-            console.log("test :", $scope.results, i);
+            $scope.results.push({
+                smallThumbnail: books[i].volumeInfo.imageLinks.smallThumbnail,
+                title: books[i].volumeInfo.title,
+                author: books[i].volumeInfo.authors[0],
+                isbn: books[i].volumeInfo.industryIdentifiers[0].identifier,
+                publisher: books[i].volumeInfo.publisher,
+                publisherDate: books[i].volumeInfo.publishedDate
+            })
+
+
+            console.log("results :", $scope.results, i);
         }
-
-
-
-
-
-        drawtheresult();
+        /*    drawtheresult(); not needed for now maybe in a future implementation  */
     };
 
 
+    /*
 
+        function drawtheresult() {
+            console.log("drawing the table with results"); // should call the ng-include
 
-    function drawtheresult() {
-        console.log("drawing the table with results");
-
-        /*
-              document.getElementsByClassName("returntop")[0].setAttribute("title", "Return to the top")*/
-
-
-    };
+        };*/
 
 }]);
